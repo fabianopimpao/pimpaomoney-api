12dc3e5505f1f2cc91ee2bbc820386af8b433364
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-09-13 01:06:26 -0300

    First commit

---
+++ src/main/java/me/pimpao/pimpaomoney/api/PimpaomoneyApiApplication.java
@@ -0,0 +1,13 @@
+package me.pimpao.pimpaomoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimpaomoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimpaomoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/pimpao/pimpaomoney/api/PimpaomoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.pimpao.pimpaomoney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimpaomoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
