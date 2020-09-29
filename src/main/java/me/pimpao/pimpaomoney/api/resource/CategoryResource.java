4ca07231f68568f4f36a4d8b8177841a5fc22afe
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-09-29 07:56:01 -0300

    Inserting new category with post method

--- src/main/java/me/pimpao/pimpaomoney/api/resource/CategoryResource.java
+++ src/main/java/me/pimpao/pimpaomoney/api/resource/CategoryResource.java
@@ -3,10 +3,11 @@ package me.pimpao.pimpaomoney.api.resource;
 import me.pimpao.pimpaomoney.api.model.Category;
 import me.pimpao.pimpaomoney.api.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +20,21 @@ public class CategoryResource {
     @GetMapping
     public List<Category> listAll() {
         return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        Category newCategory = categoryRepository.save(category);
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequestUri()
+                    .path("/{id}")
+                    .buildAndExpand(newCategory.getId())
+                    .toUri();
+        return ResponseEntity.created(uri).body(newCategory);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Category findById(@PathVariable Long id) {
+        return categoryRepository.findById(id).orElse(null);
     }
 }
