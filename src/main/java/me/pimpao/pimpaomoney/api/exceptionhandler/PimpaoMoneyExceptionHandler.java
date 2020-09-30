9c1714b7db90a46d2693b497aa70b474d07f0336
Fabiano Pimpão <fabiano.pimpao@gmail.com>
2020-09-30 00:44:55 -0300

    Errors tratament with exception handler

---
+++ src/main/java/me/pimpao/pimpaomoney/api/exceptionhandler/PimpaoMoneyExceptionHandler.java
@@ -0,0 +1,45 @@
+package me.pimpao.pimpaomoney.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class PimpaoMoneyExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
+        String developerMessage = ex.getCause().toString();
+
+        return handleExceptionInternal(ex, new Error(userMessage, developerMessage), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private static class Error {
+        private String userMessage;
+        private String developerMessage;
+
+        public Error(String userMessage, String developerMessage) {
+            this.userMessage = userMessage;
+            this.developerMessage = developerMessage;
+        }
+
+        public String getUserMessage() {
+            return userMessage;
+        }
+
+        public String getDeveloperMessage() {
+            return developerMessage;
+        }
+    }
+}
